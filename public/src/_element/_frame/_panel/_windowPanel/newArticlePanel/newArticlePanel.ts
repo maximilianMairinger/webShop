1efd2747f31bbebeebc92f3b5c658770b22627b2
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2019-03-20 23:38:46 +0100

    Feedback on Article addition

--- public/src/_element/_frame/_panel/_windowPanel/newArticlePanel/newArticlePanel.ts
+++ public/src/_element/_frame/_panel/_windowPanel/newArticlePanel/newArticlePanel.ts
@@ -1,16 +1,19 @@
 import WindowPanel from "./../windowPanel";
 import NewArticle from "../../../../_window/newArticleWindow/newArticleWindow";
 import post from "../../../../../lib/post/post";
+import Notifier from "../../../../../lib/notifier/notifier";
 
 export default class FeedbackPanel extends WindowPanel {
   constructor(blurCallback?: Function) {
     super(blurCallback);
-    this.window = new NewArticle((name, price, weight, stock, description) => {
-      post("addArticle", {
+    this.window = new NewArticle(async (name, price, weight, stock, description) => {
+      let res = await post("addArticle", {
         body: {
           name, price, weight, stock, description
         }
       });
+      if (res.suc) Notifier.success(true, "Your Article has successfully been added to the Collection");
+      else Notifier.error(true, "Ouch, something didnt work there. Try with different data again.");
     });
   }
 
